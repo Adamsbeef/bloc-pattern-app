67888f37939f7fb5ba3e4705d16d273eafd57783
Mike <blakesmike12@gmail.com>
2020-09-13 17:40:47 +0100

    initial commit

---
+++ testing_out_bloc/android/app/src/main/kotlin/com/example/testing_out_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testing_out_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testing_out_bloc/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:testing_out_bloc/src/app.dart';
+
+void main() {
+  runApp(App());
+}
+
+
+
+
